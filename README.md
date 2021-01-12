62a94631e52fbdb5d6fa22a272d044e28cfcf5ad
Daniel Runningen <daniel.runningen+github@outlook.com>
2021-01-12 22:10:23 +0000

    Adding a package for test execution time limits

--- README.md
+++ README.md
@@ -6,3 +6,9 @@ The coding examples here have been taken form external sources, including websit
 I'll do my best to credit the sources where applicable.
 If you've come looking for solutions for these problems for your own assessments, please attempt them yourself before utilizing this repo.
 Your next employer wants to see how you tackle the problem!
+
+## Running Locally
+
+Many of my solutions will be written in Python because of how fast it is to script with.
+To run the python examples locally you'll want Python (3.8 or higher is preferred), `pip3`, and `pipenv` all installed.
+Run `pipenv install` at the base of the repo and make sure to either point your IDE at the virtual environment created or run `pipenv shell` before running any tests.
--- firstDuplicate/test.py
+++ firstDuplicate/test.py
@@ -1,5 +1,8 @@
 import unittest
+import timeout_decorator
 from solution import firstDuplicate
+
+EXECUTION_TIME_LIMIT = 4
 
 # All tests in this class come directly from CodeSignal
 class CodeSignalTests(unittest.TestCase):
@@ -8,64 +11,82 @@ class CodeSignalTests(unittest.TestCase):
         self.example2 = [2, 2]
         self.example3 = [2, 4, 3, 5, 1]
 
+    @timeout_decorator.timeout(EXECUTION_TIME_LIMIT)
     def test_1(self):
         expected = 3
         actual = firstDuplicate(self.example1)
         self.assertEqual(actual, expected)
 
+    @timeout_decorator.timeout(EXECUTION_TIME_LIMIT)
     def test_2(self):
         expected = 2
         actual = firstDuplicate(self.example2)
         self.assertEqual(actual, expected)
 
+    @timeout_decorator.timeout(EXECUTION_TIME_LIMIT)
     def test_3(self):
         expected = -1
         actual = firstDuplicate(self.example3)
         self.assertEqual(actual, expected)
 
+    @timeout_decorator.timeout(EXECUTION_TIME_LIMIT)
     def test_4(self):
         expected = -1
         actual = firstDuplicate([1])
         self.assertEqual(actual, expected)
 
+    @timeout_decorator.timeout(EXECUTION_TIME_LIMIT)
     def test_5(self):
         expected = 5
         actual = firstDuplicate([5, 5, 5, 5, 5])
         self.assertEqual(actual, expected)
 
+    @timeout_decorator.timeout(EXECUTION_TIME_LIMIT)
     def test_6(self):
         expected = -1
         actual = firstDuplicate([2, 1])
         self.assertEqual(actual, expected)
 
+    @timeout_decorator.timeout(EXECUTION_TIME_LIMIT)
     def test_7(self):
         expected = -1
         actual = firstDuplicate([2, 1, 3])
         self.assertEqual(actual, expected)
 
+    @timeout_decorator.timeout(EXECUTION_TIME_LIMIT)
     def test_8(self):
         expected = 3
         actual = firstDuplicate([2, 3, 3])
         self.assertEqual(actual, expected)
 
+    @timeout_decorator.timeout(EXECUTION_TIME_LIMIT)
     def test_9(self):
         expected = 3
         actual = firstDuplicate([3, 3, 3])
         self.assertEqual(actual, expected)
 
+    @timeout_decorator.timeout(EXECUTION_TIME_LIMIT)
     def test_10(self):
         expected = 6
         actual = firstDuplicate([8, 4, 6, 2, 6, 4, 7, 9, 5, 8])
         self.assertEqual(actual, expected)
 
+    @timeout_decorator.timeout(EXECUTION_TIME_LIMIT)
     def test_11(self):
         expected = -1
         actual = firstDuplicate([10, 6, 8, 4, 9, 1, 7, 2, 5, 3])
         self.assertEqual(actual, expected)
 
+    @timeout_decorator.timeout(EXECUTION_TIME_LIMIT)
     def test_12(self):
         expected = 1
         actual = firstDuplicate([1, 1, 2, 2, 1])
+        self.assertEqual(actual, expected)
+
+    @timeout_decorator.timeout(EXECUTION_TIME_LIMIT)
+    def test_emptyList(self):
+        expected = -1
+        actual = firstDuplicate([])
         self.assertEqual(actual, expected)
 
 if __name__ == '__main__':
